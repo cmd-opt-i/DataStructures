c8f9eebc36a082ca195d4bb67b748a5d6f2a80b6
Irving <Irvingb232@gmail.com>
2016-11-26 22:31:09 -0800

    [adds] hash table add function

--- Hash_Table.js
+++ Hash_Table.js
@@ -15,3 +15,19 @@ function HashTable(size) {
   this.numberOfValues = 0;
   this.size = size;
 }
+
+HashTable.prototype.add = function(key, value) {
+  var hash = this.calculateHash(key);
+
+  // if hash has not been used create new bucket
+  if (!this.values.hasOwnProperty(hash)) {
+    this.values[hash] = {};
+  }
+  // increment 'numberOfValues' if key is not being used
+  if (!this.values[hash].hasOwnProperty(key)) {
+    this.numberOfValues++;
+  }
+
+  // store 'value' in hash bucket
+  this.values[hash][key] = value;
+};
