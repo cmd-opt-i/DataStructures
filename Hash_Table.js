0c5d23a8974ebeec4c2193ca477719bf2af7740f
Irving <Irvingb232@gmail.com>
2016-11-26 22:26:01 -0800

    [adds] hash table description

---
+++ Hash_Table.js
@@ -0,0 +1,12 @@
+/*
+  Definition:
+    A Hash Table (Hash Map) is a data structure used to implement an associative array, a structure that can map keys to values. A Hash Table uses a hash function to compute an index into an array of buckets or slots, from which the desired value can be found.
+    
+    Note: Considered the more efficient data structure for lookup.
+
+  Complexity:
+    Average      
+    Access  Search  Insertion Deletion
+      -      O(1)      O(1)     O(1)
+*/
+
