681df22aede24f50ab46c4e079adfdc2014fffec
Irving <Irvingb232@gmail.com>
2016-11-27 14:08:34 -0800

    [add] the singly linked list solution

--- The_Singly_Linked_List.js
+++ The_Singly_Linked_List.js
@@ -9,3 +9,115 @@
     Access  Search  Insertion Deletion
      O(n)    O(n)      O(1)     O(1)
 */
+
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function SinglyLinkedList() {
+  this.head = null;
+  this.tail = null;
+  this.numberOfValues = 0;
+}
+
+SinglyLinkedList.prototype.add = function(data) {
+  var node = new Node(data);
+
+  // 1st node, add to 'head' and 'tail'
+  if (!this.head) {
+    this.head = node;
+    this.tail = node;
+  } else {
+    // point prev 'tail's next to new node, and tail to new node
+    this.tail.next = node;
+    this.tail = node;
+  }
+
+  this.numberOfValues++;
+};
+
+SinglyLinkedList.prototype.remove = function(data) {
+  var previous = this.head;
+  var current = this.head;
+
+  while (current) {
+    // data is found
+    if (current.data === data) {
+      
+      // if this is a 'head', make new 'head' point to next
+      if (current === this.head) {
+        this.head = this.head.next;
+      }
+
+      // if this is a 'tail', make new tail point to the previous node
+      if (current === this.tail) {
+        this.tail = previous;
+      }
+
+      // connect list and keep searching
+      previous.next = current.next;
+      this.numberOfValues--;
+    } else {
+      // data not found
+      previous = current;
+    }
+
+    // search next node
+    current = current.next;
+  }
+};
+
+SinglyLinkedList.prototype.insertAfter = function(data, toNodeData) {
+  var current = this.head;
+
+  while (current) {
+
+    if (current.data === toNodeData) {
+      var node = new Node(data);
+
+      if (current === this.tail) {
+        this.tail.next = node;
+        this.tail = node;
+      } else {
+        node.next = current.next;
+        current.next = node;
+      }
+
+      this.numberOfValues++;
+    }
+
+    current = current.next;
+  }
+};
+
+SinglyLinkedList.prototype.traverse = function(fn) {
+  var current = this.head;
+
+  while (current) {
+
+    if (fn) {
+      fn(current);
+    }
+
+    current = current.next;
+  }
+};
+
+SinglyLinkedList.prototype.length = function() {
+  return this.numberOfValues;
+};
+
+SinglyLinkedList.prototype.print = function() {
+  var string = '';
+  var current = this.head;
+
+  while (current) {
+    string += current.data + ' ';
+    current = current.next;
+  }
+
+  console.log(string.trim());
+};
+
+var singlyLinkedList = new SinglyLinkedList();
