eab54524548e4be0a9f7717736d155fe00d55ef3
Irving <Irvingb232@gmail.com>
2016-11-26 22:54:03 -0800

    [adds] insert, retrieve, and remove functions

--- Hash_Table.js
+++ Hash_Table.js
@@ -10,24 +10,68 @@
       -      O(1)      O(1)     O(1)
 */
 
-function HashTable(size) {
-  this.values = {};
-  this.numberOfValues = 0;
-  this.size = size;
+var HashTable = function(size) {
+  this.storage = [];
+  this.storageLimit = size;
 }
 
-HashTable.prototype.add = function(key, value) {
-  var hash = this.calculateHash(key);
+HashTable.prototype.insert = function(key, value) {
+  var index = getIndexBelowMaxForKey(key, this.storageLimit);
 
-  // if hash has not been used create new bucket
-  if (!this.values.hasOwnProperty(hash)) {
-    this.values[hash] = {};
-  }
-  // increment 'numberOfValues' if key is not being used
-  if (!this.values[hash].hasOwnProperty(key)) {
-    this.numberOfValues++;
-  }
+  // create a new bucket
+  if (this.storage[index] === undefined) {
+    this.storage[index] = [[key, value]];
+  } else {
+    var inserted = false;
 
-  // store 'value' in hash bucket
-  this.values[hash][key] = value;
+    // checks all tuples inside of this bucket for that 'key'
+    // Ex: storage[[key, value], [key, value], [key, value]];
+    for (var i = 0; i < this.storage[index].length; i++) {
+      // overwrite key and turn on flag
+      if (this.storage[index][i][0] === key) {
+        storage[index][i][1] = value;
+        inserted = true;
+      }
+    }
+
+    // if tuple does not exist push new tuple
+    // Ex: tuple = [key, value]
+    if (inserted === false) {
+      storage[index].push([key,value]);
+    }
+  }
+};
+
+HashTable.prototype.retrieve = function(key) {
+  var index = getIndexBelowMaxForKey(key, this.storageLimit);
+
+  // bucket does not exist return 'undefined'
+  if (storage[index] === undefined) {
+    return undefined;
+  } else {
+
+    // search tuples in bucket for the specific 'key'
+    for (var i = 0; i < storage[index].length; i++) {
+      if (storage[index][i][0] === key) {
+        return storage[index][i][1];
+      }
+    }
+  }
+};
+
+HashTable.prototype.remove = function(key) {
+  var index = getIndexBelowMaxForKey(key, this.storageLimit);
+
+  // if bucket only has one tuple with matching 'key', delete
+  if (storage[index].length === 1 && storage[index][0][0] === key) {
+    delete storage[index];
+  } else {
+
+    // search through all tuples and remove matching 'key'
+    for (var i = 0; i < storage[index]; i++) {
+      if (storage[index][i][0] ===  key) {
+        delete storage[index][i];
+      }
+    }
+  }
 };
