df1df33fd9807cf6ab1158c52c6692a913488996
Irving <Irvingb232@gmail.com>
2016-11-26 23:02:25 -0800

    [adds] hash function

--- Hash_Table.js
+++ Hash_Table.js
@@ -75,3 +75,15 @@ HashTable.prototype.remove = function(key) {
     }
   }
 };
+
+var getIndexBelowMaxForKey = function(str, max) {
+  var hash = 0;
+
+  for (var i = 0; i < str.length; i++) {
+    hash = (hash<<5) + hash + str.charCodeAt(i);
+      hash = hash & hash; // Convert to 32bit integer
+    hash = Math.abs(hash);
+  }
+  
+  return hash % max;
+};
